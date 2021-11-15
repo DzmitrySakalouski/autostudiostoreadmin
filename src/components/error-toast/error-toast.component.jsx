87c56c816de3bce2d1ea91c8968446db4bb89dbd
DzmitrySakalouski <dmitry89sokol@gmail.com>
2021-11-15 18:35:50 +0300

    refactor: toast

--- src/components/error-toast/error-toast.component.jsx
+++ src/components/error-toast/error-toast.component.jsx
@@ -1,5 +1,5 @@
 import { Snackbar, Alert } from "@mui/material";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 
 export const ErrorToast = ({ errorMessage }) => {
@@ -23,9 +23,11 @@ export const ErrorToast = ({ errorMessage }) => {
         setErrors([]);
     }, []);
 
+    const shoulShowToast = useMemo(() => errors.length && errors[errors.length - 1], [errors]);
+
     if (errors && errors.length) {
         return (
-            <Snackbar open={errors.length && errors[errors.length - 1]} autoHideDuration={300}>
+            <Snackbar open={shoulShowToast} autoHideDuration={300}>
                 <Alert onClose={handleClose} severity="error">{errors[errors.length - 1]}</Alert>
             </Snackbar>
         );
