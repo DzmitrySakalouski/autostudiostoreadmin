345f40d96397e03698b04b1118ca5157d6cfc10a
DzmitrySakalouski <dmitry89sokol@gmail.com>
2021-11-15 12:58:17 +0300

    feat: add login screen

--- src/view/login/login.view.jsx
+++ src/view/login/login.view.jsx
@@ -1,10 +1,41 @@
-import TextField from '@mui/material/TextField';
+import { TextField, Paper, Button } from '@mui/material';
+import {makeStyles} from '@mui/styles';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+
+const useStyles = makeStyles({
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        height: '100vh',
+    },
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: 20,
+        height: 300,
+        maxWidth: 400,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    button: {
+        width: '100%',
+        height: 50,
+    }
+});
 
 export const LoginView = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <TextField label="Username" />
-            <TextField label="Password" />
+        <div className={classes.container}>
+            <Paper className={classes.paper}>
+                <LockOpenIcon sx={{fontSize: 60}} />
+                <TextField label="Email" />
+                <TextField label="Password" />
+                <Button className={classes.button} variant="contained">Login</Button>
+            </Paper>
+
         </div>
     );
 }
