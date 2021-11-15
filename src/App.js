5331bccf530704c0dbf082081848a6621e12404c
DzmitrySakalouski <dmitry89sokol@gmail.com>
2021-11-15 14:53:17 +0300

    fix: markup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { MainNavigator } from "./navigation/main";
+import './index.scss'
 
 function App() {
   return (
--- src/view/login/login.view.jsx
+++ src/view/login/login.view.jsx
@@ -1,6 +1,9 @@
 import { TextField, Paper, Button } from '@mui/material';
-import {makeStyles} from '@mui/styles';
+import { makeStyles } from '@mui/styles';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
+import { useForm, Controller } from 'react-hook-form';
+import { EMAIL_REGEX } from '../../constants';
+import { useEffect } from 'react';
 
 const useStyles = makeStyles({
     container: {
@@ -27,13 +30,74 @@ const useStyles = makeStyles({
 
 export const LoginView = () => {
     const classes = useStyles();
+    const { control, handleSubmit, formState: {errors} } = useForm();
+
+    useEffect(() => {
+        console.log(errors);
+
+    }, [errors])
+
+    
+
+    const onSubmit = (data) => console.log(data);
+
     return (
         <div className={classes.container}>
             <Paper className={classes.paper}>
-                <LockOpenIcon sx={{fontSize: 60}} />
-                <TextField label="Email" />
-                <TextField label="Password" />
-                <Button className={classes.button} variant="contained">Login</Button>
+                <LockOpenIcon sx={{ fontSize: 60 }} />
+                <Controller
+                    name="email"
+                    control={control}
+                    defaultValue=""
+                    rules={{
+                        required: {
+                            value: true,
+                            message: "Это поле не может быть пустым"
+                        },
+                        pattern: {
+                            value: EMAIL_REGEX,
+                            message: "Ошибка в емейле"
+                        }
+                    }}
+                    render={({field: {value, onChange, onBlur} }) => {
+                        return <TextField
+                            label="Email"
+                            onChange={onChange}
+                            onBlur={onBlur}
+                            value={value}
+                            error={errors?.email}
+                            helperText={errors?.email?.message} />
+                    }
+                    }
+                />
+                <Controller
+                    name="password"
+                    control={control}
+                    defaultValue=""
+                    rules={{
+                        required: {
+                            value: true,
+                            message: "Это поле не может быть пустым"
+                        }
+                    }}
+                    render={({field: { onChange, value, onBlur }}) =>
+                        <TextField
+                            label="Password"
+                            onChange={onChange}
+                            value={value}
+                            type="password"
+                            onBlur={onBlur}
+                            error={errors?.password?.message}
+                            helperText={errors?.password?.message} />
+                    }
+                />
+
+                <Button
+                    className={classes.button}
+                    onClick={handleSubmit(onSubmit)}
+                    variant="contained">
+                    Login
+                </Button>
             </Paper>
 
         </div>
