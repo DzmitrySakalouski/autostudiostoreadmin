5faffc5cd56ebeb44735a3182aa8227fef8abc4f
DzmitrySakalouski <dmitry89sokol@gmail.com>
2021-11-17 16:48:15 +0300

    add product list

--- src/view/home-dashboard/components/product-group.component.jsx
+++ src/view/home-dashboard/components/product-group.component.jsx
@@ -1,8 +1,31 @@
-export const ProductGroupItem = ({name, description}) => {
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export const ProductGroupItem = ({ name, description, image }) => {
     return (
-        <div>
-            <h1>{name}</h1>
-            <h1>{description}</h1>
-        </div>
-    )
+        <Card sx={{ width: 325 }} style={{}}>
+            <CardMedia
+                component="img"
+                height="180"
+                image={image}
+                alt="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Редактировать</Button>
+                <Button size="small">Подробнее</Button>
+            </CardActions>
+        </Card>
+    );
 }
--- src/view/home-dashboard/components/product-groups-list.component.jsx
+++ src/view/home-dashboard/components/product-groups-list.component.jsx
@@ -1,8 +1,19 @@
+import { Grid } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Box } from "@mui/system";
 import { useSelector } from "react-redux";
 import { ProductGroupItem } from "./product-group.component";
 
+const useStyles = makeStyles({
+    conatainer: {
+        padding: "16px",
+    }
+})
+
 export const ProductGroupList = () => {
-    const {groups} = useSelector(state => state.productGroups);
+    const { groups } = useSelector(state => state.productGroups);
+    const classes = useStyles();
+
     if (!groups?.length) {
         return (
             <h1>Групп нет</h1>
@@ -10,10 +21,15 @@ export const ProductGroupList = () => {
     }
 
     return (
-        <div>
-            {groups.map(group => (
-                <ProductGroupItem key={group.id} name={group.name} description={group.description} />
-            ))}
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }} className={classes.conatainer}>
+                {groups.map(group => (
+                    <Grid item >
+                        <ProductGroupItem key={group.id} name={group.name} image={group.imageUrl} description={group.description} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+
     )
 }
